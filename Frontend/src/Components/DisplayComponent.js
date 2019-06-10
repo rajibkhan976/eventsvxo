b32b763db7c1721d3bdaaee86cf81350719d0048
valentinapalma <valentinapalma@hotmail.se>
2019-06-10 12:49:17 +0200

    react components added

---
+++ Frontend/src/Components/DisplayComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DisplayComponent extends Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default DisplayComponent;

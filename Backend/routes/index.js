9131296e972c216a07fc4067b23b7e0d7d910bb2
unknown <rajibkhan976@gmail.com>
2019-06-13 12:21:01 +0200

    update event method implemented

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const event = require("./eventMethods.js");
 
 router.post("/events", event.addEvent);
+router.get("/events", event.getEvents);
+router.patch("/events/:id", event.updateEventById)
 
 module.exports = router;

82a86f69442f84b0596a7dc15b376b0db1fff6e1
unknown <rajibkhan976@gmail.com>
2019-06-13 10:40:32 +0200

    event model fixed

--- Backend/models/events.js
+++ Backend/models/events.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
-    
-})
+    title: String,
+    start_date: {
+      year: Number,
+      month: Number,
+      day: Number,
+      time: String
+    },
+    end_date: {
+      year: Number,
+      month: Number,
+      day: Number,
+      time: String
+    },
+    location: String,
+    price: String,
+    category: String,
+    tags: String,
+    additional_info: String,
+    host: String,
+    img: String
+});
 
-const Events = mongoose.model('Events', eventSchema)
+const Events = mongoose.model('Events', eventSchema);
 
 module.exports = Events;
